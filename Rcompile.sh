27ddb97f57b36f42c05a2b909363b04136634243
!MikaSukie(미카수키)! <75727770+MikaSukie@users.noreply.github.com>
2026-04-25 14:14:37 +0000

    Add files via upload

---
+++ Rcompile.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+clear
+  ./rosaline main.ros -o out.ll &&
+  opt -O2 out.ll -o out.opt.ll &&
+  clang -O2 -Wno-override-module out.opt.ll stdlib.c -o main &&
+time bash -c '  ./main
+  echo "Exit code: $?"
+'
